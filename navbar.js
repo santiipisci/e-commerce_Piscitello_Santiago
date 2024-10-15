1b6c15b553ee74dda40f6a66a2bbae07399c9aa2
pepe <pepe@pepe.com>
2024-10-15 19:07:24 -0300

    terminado

--- navbar.js
+++ navbar.js
@@ -3,48 +3,59 @@ let categorias = [
     texto: "Home", href: "/index.html"
   },
   {
-    texto: "Producto", href: "/producto.html"
+    texto: "Perifericos", href: "#"
   },
   {
-    texto: "Contacto", href: "/contacto.html"
+    texto: "Tarjetas Graficas", href: "#"
   },
+  {
+    texto: "Procesadores", href: "#"
+  },
+  {
+    texto: "Memorias", href: "#"
+  },
+  {
+    texto: "Discos HDD/SSD", href: "#"
+  },
+  
 ]
 let header = document.querySelector("header");
 let arrayHTML = [];
 
 let navbar = `
 <nav class="navbar bg-body-tertiary fixed-top">
-                <div class="container-fluid">
-                  <a class="navbar-brand" href="index.html">SP Componentes</a>
-                  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                    <div class="offcanvas-body">
-                      <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                          <ul class="dropdown-menu">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            <li><a class="dropdown-item" href="#">Perifericos</a></li>
-                            <li><a class="dropdown-item" href="#">Tarjetas Graficas</a></li>
-                            <li><a class="dropdown-item" href="#">Procesadores</a></li>
-                            <li><a class="dropdown-item" href="#">Memorias</a></li>
-                            <li><a class="dropdown-item" href="#">Discos HDD/SSD</a></li>
-                            <form class="d-flex mt-3" role="search">
-                              <div class="container-fluid">
-                                <form class="d-flex" role="search">
-                                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                  <button class="btn btn-outline-success" type="submit">Search</button> 
-                                  <button onclick="tuCarrito()">🛒 Carrito</button>
-                                </form>
-                              </div>
-                              <hr class="dropdown-divider">
-                          </ul>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </nav>
-              `;
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.html">SP Componentes</a>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li>
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <div class="container-fluid">
+            <div>
+              <input class="input" type="search" placeholder="Search" aria-label="Search">
+              <button class="search" type="submit">Search</button> 
+              <button class="reset" type="submit">X</button> 
+              <button onclick="tuCarrito()">🛒 Carrito</button>
+            </div>
+          </div>
+          <hr class="dropdown-divider">
+          <ul class="dropdown-menu categorias-lista">
+          </ul>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+`;
+
+header.innerHTML = navbar
 
 for (let elemen of categorias) {
-  arrayHTML.push(`<a class="nav-item" aria-current="page" href=${elemen.href}> ${elemen.texto} </a>`)
+  arrayHTML.push(`<li><button class="btncategorias"><a href="${elemen.href}" class="nav-link active">${elemen.texto}</a></button></li>`)
 }
 
-header.innerHTML = navbar;
+document.querySelector(".categorias-lista").innerHTML=arrayHTML.join("")
+
+
